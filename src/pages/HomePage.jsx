b20c1b86fcc9b541a1722412e417262ae41db82a
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2024-01-27 11:11:29 +0530

    Setting up Inital React Dom Routing

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>WorldWise</h1>
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
+};
+
+export default HomePage;
